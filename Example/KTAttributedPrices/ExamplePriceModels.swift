c9c343e02b8dcf665ea08013a06e68a57ef1e230
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-07-11 00:25:01 -0400

    sample models

---
+++ Example/KTAttributedPrices/ExamplePriceModels.swift
@@ -0,0 +1,23 @@
+//
+//  ExamplePriceModels.swift
+//  KTAttributedPrices
+//
+//  Created by Kevin Taniguchi on 7/11/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct SinglePrice {
+    let listPrice: Double
+    let salePrice: Double
+    let promo: String?
+}
+
+struct RangeablePrice {
+    let listLow: Double
+    let listHigh: Double
+    let saleLow: Double
+    let saleHigh: Double
+    let promo: String?
+}
